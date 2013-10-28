1f8e9bae9c1d3f95665fd7fcf3899ae8969e774d
Ron Buchanan <ron.buchanan.us@gmail.com>
2013-10-28 19:42:34 -0400

    Revert bb2fe60..6fb5d75
    This rolls back to commit bb2fe6024ffc08c5c6de88955883ec684f554f18.

--- templates/javascript/service/service.js
+++ templates/javascript/service/service.js
@@ -1,6 +1,6 @@
 'use strict';
 
 angular.module('<%= _.camelize(appname) %>App')
-  .service('<%= _.classify(name) %>', function <%= _.classify(name) %>() {
+  .service('<%= name %>', function <%= name %>() {
     // AngularJS will instantiate a singleton by calling "new" on this function
   });
