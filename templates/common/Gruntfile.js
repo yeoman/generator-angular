1afa3297fc6d8b3b71bde210faca855c46b6518e
kirill <kirill>
2013-12-08 04:21:34 -0500

    Changed grunt development server port to 9003 to combat common port collisions with other applicaitons

--- templates/common/Gruntfile.js
+++ templates/common/Gruntfile.js
@@ -73,7 +73,7 @@ module.exports = function (grunt) {
     // The actual grunt server settings
     connect: {
       options: {
-        port: 9000,
+        port: 9003,
         // Change this to '0.0.0.0' to access the server from outside.
         hostname: 'localhost',
         livereload: 35729
