ca765509b4bf3d827ac40206ea9bb84936b806cc
Pascal Hartig <passy@twitter.com>
2013-12-21 18:22:12 +0100

    fix(Gruntfile): correct attribute quoting
    Fixes #521

--- templates/common/Gruntfile.js
+++ templates/common/Gruntfile.js
@@ -203,7 +203,7 @@ module.exports = function (grunt) {
         httpFontsPath: '/styles/fonts',
         relativeAssets: false,
         assetCacheBuster: false,
-        raw: "Sass::Script::Number.precision = 10\n"
+        raw: 'Sass::Script::Number.precision = 10\n'
       },
       dist: {
         options: {
