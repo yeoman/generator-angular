1c909059a4850df88e7561719a4a3ef457f3f6bd
Eddie Monge <eddie@eddiemonge.com>
2014-01-15 12:57:55 -0800

    bug(build): deprecated grunt server didnt pass in target
    Even though this method is deprecated, it should still work correctly

--- templates/common/Gruntfile.js
+++ templates/common/Gruntfile.js
@@ -412,9 +412,9 @@ module.exports = function (grunt) {
     ]);
   });
 
-  grunt.registerTask('server', function () {
+  grunt.registerTask('server', function (target) {
     grunt.log.warn('The `server` task has been deprecated. Use `grunt serve` to start a server.');
-    grunt.task.run(['serve']);
+    grunt.task.run(['serve:' + target]);
   });
 
   grunt.registerTask('test', [
