2a93829c0b16a7c286735d0bb5f5558c7709df33
jtmkrueger <johnk@linux.com>
2014-04-08 13:18:30 -0600

    add option to choose port when firing up server
    I find myself having to change this all the time when I want to run more than 1 project at the same time. Lets make that easier!

--- templates/common/Gruntfile.js
+++ templates/common/Gruntfile.js
@@ -77,7 +77,7 @@ module.exports = function (grunt) {
     // The actual grunt server settings
     connect: {
       options: {
-        port: 9000,
+        port: grunt.option('port') || 9000,
         // Change this to '0.0.0.0' to access the server from outside.
         hostname: 'localhost',
         livereload: 35729
