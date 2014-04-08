51ee2c90a4352be19fc7d351a4b3e87d14a277c8
jtmkrueger <johnk@linux.com>
2014-04-08 13:38:42 -0600

    allow livereload port selection

--- templates/common/Gruntfile.js
+++ templates/common/Gruntfile.js
@@ -80,7 +80,7 @@ module.exports = function (grunt) {
         port: grunt.option('port') || 9000,
         // Change this to '0.0.0.0' to access the server from outside.
         hostname: 'localhost',
-        livereload: 35729
+        livereload: grunt.option('livereloadport') || 35729
       },
       livereload: {
         options: {
