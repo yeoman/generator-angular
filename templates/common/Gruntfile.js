fac3d1082a2c2120b9f92b86747c26b1421bd942
PanJ <panjmp@gmail.com>
2014-04-18 04:12:50 +0900

    fix(Gruntfile): fix usemin bug when using images in css
    Fix the bug that occurs when using images in css file.

--- templates/common/Gruntfile.js
+++ templates/common/Gruntfile.js
@@ -263,7 +263,7 @@ module.exports = function (grunt) {
       html: ['<%%= yeoman.dist %>/{,*/}*.html'],
       css: ['<%%= yeoman.dist %>/styles/{,*/}*.css'],
       options: {
-        assetsDirs: ['<%%= yeoman.dist %>']
+        assetsDirs: ['<%%= yeoman.dist %>','<%%= yeoman.dist %>/images']
       }
     },
 
