57c83f1100c6968fad2c188435e2e3c3d5712e80
ghachey <ghachey@gmail.com>
2014-05-02 10:51:15 +0800

    Change livereload from true to connect.options.livereload

--- templates/common/Gruntfile.js
+++ templates/common/Gruntfile.js
@@ -43,7 +43,7 @@ module.exports = function (grunt) {
         files: ['<%%= yeoman.app %>/scripts/{,*/}*.js'],
         tasks: ['newer:jshint:all'],
         options: {
-          livereload: true
+          livereload: '<%%= connect.options.livereload %>'
         }
       },
       jsTest: {
