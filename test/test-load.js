f90859684f3cb211508bf05f8d0ca21c9547f5f6
Eddie Monge <eddie@eddiemonge.com>
2014-05-14 13:27:15 -0700

    fix(test): add test for testing loading

---
+++ test/test-load.js
@@ -0,0 +1,23 @@
+/*global describe, beforeEach, it*/
+'use strict';
+
+var assert = require('yeoman-generator').assert;
+
+describe('Angular-Module generator load test', function () {
+  it('can be imported without blowing up', function () {
+    assert(require('../app') !== undefined);
+    assert(require('../common') !== undefined);
+    assert(require('../constant') !== undefined);
+    assert(require('../controller') !== undefined);
+    assert(require('../decorator') !== undefined);
+    assert(require('../directive') !== undefined);
+    assert(require('../factory') !== undefined);
+    assert(require('../filter') !== undefined);
+    assert(require('../main') !== undefined);
+    assert(require('../provider') !== undefined);
+    assert(require('../route') !== undefined);
+    assert(require('../service') !== undefined);
+    assert(require('../value') !== undefined);
+    assert(require('../view') !== undefined);
+  });
+});
