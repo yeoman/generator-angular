6d2d95adea435e529c321dd7feaa3a1c71e3bdff
Shahar Talmi <shahar.talmi@gmail.com>
2014-05-15 11:10:14 +0300

    minor fixes to e2e

--- templates/common/scenarios.js
+++ templates/common/scenarios.js
@@ -2,6 +2,14 @@
 
 describe('<%= _.camelize(appname) %>App', function () {
 
+  beforeEach(function () {
+    browser.addMockModule('<%= scriptAppName %>Mocks', function () {});
+  });
+
+  afterEach(function () {
+    browser.removeMockModule();
+  });
+
   it('should load successfully', function () {
     browser.get('/');
     expect(element(by.css('h3')).getText()).toEqual('Enjoy coding! - Yeoman');
