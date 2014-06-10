3084d3f59ffc442b5ff0841a92d1f32ac43c67f1
Eddie Monge <eddie@eddiemonge.com>
2014-06-10 08:59:22 -0700

    chore(release): add note about release and staged files
    Changed default task to not do much and added a release task. Hopefully
    whoever does a release, looks at the Gruntfile to see which task to run
    and sees the comment about grunt-release not working exactly as we want
    it to

--- Gruntfile.js
+++ Gruntfile.js
@@ -40,6 +40,8 @@ module.exports = function (grunt) {
     }
   });
 
+  grunt.registerTask('default', ['jshint']);
+
   grunt.registerTask('bump', 'Bump manifest version', function (type) {
     var options = this.options({
       file: grunt.config('pkgFile') || 'package.json'
@@ -71,9 +73,12 @@ module.exports = function (grunt) {
     }, grunt.task.current.async());
   });
 
-  grunt.registerTask('default', function (type) {
+  // grunt-release will only commit the package.json file by default. Until
+  // https://github.com/geddski/grunt-release/pull/43/files lands, it should
+  // be patched to do the same so it commits the changelog as well.
+  grunt.registerTask('release', function (type) {
     grunt.task.run([
-      'jshint',
+      'default',
       'bump' + (type ? ':' + type : ''),
       'changelog',
       'stage',
