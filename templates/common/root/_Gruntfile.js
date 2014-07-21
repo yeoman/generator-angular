5f71e289a443994563a05995906785edd3b1c65d
John Kitsantas <jkitsantas@alfabetaroto.com>
2014-07-21 12:45:42 +0300

    Changed ngmin to ng-annotate, but not in the README file yet...

--- templates/common/root/_Gruntfile.js
+++ templates/common/root/_Gruntfile.js
@@ -349,10 +349,7 @@ module.exports = function (grunt) {
       }
     },
 
-    // ngmin tries to make the code safe for minification automatically by
-    // using the Angular long form for dependency injection. It doesn't work on
-    // things like resolve or inject so those have to be done manually.
-    ngmin: {
+    ngAnnotate: {
       dist: {
         files: [{
           expand: true,
@@ -482,7 +479,7 @@ module.exports = function (grunt) {
     'concurrent:dist',
     'autoprefixer',
     'concat',
-    'ngmin',
+    'ngAnnotate',
     'copy:dist',
     'cdnify',
     'cssmin',
