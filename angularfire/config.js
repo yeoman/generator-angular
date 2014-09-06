91f5338aefcfafe8f24ac6933977c8704a754a3f
Mike Wilcox <mwilcox56@gmail.com>
2014-09-06 18:52:19 -0400

    changed config from json to js

---
+++ angularfire/config.js
@@ -0,0 +1,19 @@
+var config = {
+  "bower": {
+    "firebase": "1.0.x",
+    "angularfire": "0.8.x",
+    "mockfirebase": "0.2.x",
+    "simplelogin": "1.6.x"
+  },
+
+  "simpleLoginProviders": [
+    { "name": "Email/Password", "value": "password", "checked": true },
+    { "name": "Anonymous",      "value": "anonymous" },
+    { "name": "Facebook",       "value": "facebook"  },
+    { "name": "Google",         "value": "google"    },
+    { "name": "Twitter",        "value": "twitter"   },
+    { "name": "GitHub",         "value": "github"    }
+  ]
+}
+
+module.exports = config;
