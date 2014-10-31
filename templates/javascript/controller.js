c37938c7793da65ce14154f3f8a08e1a753f626d
Wesckley <wesckley.gomes@gmail.com>
2014-10-31 18:10:28 -0300

    add dependencie in the controller

--- templates/javascript/controller.js
+++ templates/javascript/controller.js
@@ -8,10 +8,10 @@
  * Controller of the <%= scriptAppName %>
  */
 angular.module('<%= scriptAppName %>')
-  .controller('<%= classedName %>Ctrl', function ($scope) {
+  .controller('<%= classedName %>Ctrl', ['$scope', function ($scope) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
-  });
+  }]);
