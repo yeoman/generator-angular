2df543a00bf19ea006214aacc8fa8193feeb1fc3
Alexandre <alexand.bonhom@gmail.com>
2014-10-31 16:34:19 +0100

    Adds closure to controller template

--- templates/javascript/controller.js
+++ templates/javascript/controller.js
@@ -1,17 +1,22 @@
-'use strict';
+(function() {
+  'use strict';
 
-/**
- * @ngdoc function
- * @name <%= scriptAppName %>.controller:<%= classedName %>Ctrl
- * @description
- * # <%= classedName %>Ctrl
- * Controller of the <%= scriptAppName %>
- */
-angular.module('<%= scriptAppName %>')
-  .controller('<%= classedName %>Ctrl', function ($scope) {
+  /**
+   * @ngdoc function
+   * @name <%= scriptAppName %>.controller:<%= classedName %>Ctrl
+   * @description
+   * # <%= classedName %>Ctrl
+   * Controller of the <%= scriptAppName %>
+   */
+  angular
+    .module('<%= scriptAppName %>')
+    .controller('<%= classedName %>Ctrl', <%= classedName %>);
+
+  function <%= classedName %>($scope) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
-  });
+  }
+})();
