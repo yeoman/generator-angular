7a65321fd6fdbce0b852f08d0796c4bd6181b7a9
Alexandre <alexand.bonhom@gmail.com>
2014-10-31 17:02:33 +0100

    Adds closures to decorator.js

--- templates/javascript/decorator.js
+++ templates/javascript/decorator.js
@@ -1,16 +1,21 @@
-'use strict';
+(function() {
+	'use strict';
 
-/**
- * @ngdoc function
- * @name <%= scriptAppName %>.decorator:<%= classedName %>
- * @description
- * # <%= classedName %>
- * Decorator of the <%= scriptAppName %>
- */
-angular.module('<%= scriptAppName %>')
-  .config(function ($provide) {
-    $provide.decorator('<%= cameledName %>', function ($delegate) {
-      // decorate the $delegate
-      return $delegate;
-    });
-  });
+	/**
+	 * @ngdoc function
+	 * @name <%= scriptAppName %>.decorator:<%= classedName %>
+	 * @description
+	 * # <%= classedName %>
+	 * Decorator of the <%= scriptAppName %>
+	 */
+	angular
+		.module('<%= scriptAppName %>')
+	  .config(function ($provide) {
+	    $provide.decorator('<%= cameledName %>', <%= cameledName %>);
+	  });
+
+	function <%= cameledName %>($delegate) {
+    // decorate the $delegate
+    return $delegate;
+  }
+})();
