b3d09f030b452761320c065417ae607bdc7fb251
Marcin Wosinek <marcin.wosinek@gmail.com>
2015-02-03 11:00:05 +0000

    refactor(gen): read bower.json once

--- script-base.js
+++ script-base.js
@@ -8,40 +8,31 @@ var chalk = require('chalk');
 var Generator = module.exports = function Generator() {
   yeoman.generators.NamedBase.apply(this, arguments);
 
-  try {
-    this.appname = require(path.join(process.cwd(), 'bower.json')).name;
-  } catch (e) {
-    this.appname = path.basename(process.cwd());
-  }
-  this.appname = this._.slugify(this._.humanize(this.appname));
+  var bowerJson = {};
 
   try {
-    this.scriptAppName = require(path.join(process.cwd(), 'bower.json')).moduleName;
+    bowerJson = require(path.join(process.cwd(), 'bower.json'));
   } catch (e) {}
 
-  this.scriptAppName = this.scriptAppName || this._.camelize(this.appname) + angularUtils.appName(this);
+  if (bowerJson.name) {
+    this.appname = bowerJson.name;
+  } else {
+    this.appname = path.basename(process.cwd());
+  }
+
+  this.appname = this._.slugify(this._.humanize(this.appname));
+
+  this.scriptAppName = bowerJson.moduleName || this._.camelize(this.appname) + angularUtils.appName(this);
 
   this.cameledName = this._.camelize(this.name);
   this.classedName = this._.classify(this.name);
 
   if (typeof this.env.options.appPath === 'undefined') {
-    this.env.options.appPath = this.options.appPath;
-
-    if (!this.env.options.appPath) {
-      try {
-        this.env.options.appPath = require(path.join(process.cwd(), 'bower.json')).appPath;
-      } catch (e) {}
-    }
-    this.env.options.appPath = this.env.options.appPath || 'app';
+    this.env.options.appPath = this.options.appPath || bowerJson.appPath || 'app';
     this.options.appPath = this.env.options.appPath;
   }
 
-  if (typeof this.env.options.testPath === 'undefined') {
-    try {
-      this.env.options.testPath = require(path.join(process.cwd(), 'bower.json')).testPath;
-    } catch (e) {}
-    this.env.options.testPath = this.env.options.testPath || 'test/spec';
-  }
+  this.env.options.testPath = this.env.options.testPath || bowerJson.testPath || 'test/spec';
 
   this.env.options.coffee = this.options.coffee;
   if (typeof this.env.options.coffee === 'undefined') {
