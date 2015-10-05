427d1d139ac4d6f871b3dd199e409a595ac38388
Alberto López-Barranco <alberto.lopezbarranco@gmail.com>
2015-10-05 14:46:14 +0200

    fix(gulp): fix typo in the `build` task
    Close #1187

--- templates/common/root/_gulpfile.js
+++ templates/common/root/_gulpfile.js
@@ -213,7 +213,7 @@ gulp.task('copy:fonts', function () {
 });
 
 gulp.task('build', ['clean:dist'], function () {
-  runSequence(['images', 'copy:extras', 'copy:fonts', 'client:build');
+  runSequence(['images', 'copy:extras', 'copy:fonts', 'client:build']);
 });
 
 gulp.task('default', ['build']);
